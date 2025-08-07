c98b4608f1e3bdff26bd4fda67a2f437ada9a9c3
Cesar Augusto Diaz Arriaga <cesardevsenior@gmail.com>
2025-08-06 21:01:37 -0500

    Update package name and main class name

--- src/main/java/com/devsenior/cdiaz/property/service/PropertyServiceApplication.java
+++ src/main/java/com/devsenior/cdiaz/property/service/PropertyServiceApplication.java
@@ -1,13 +1,13 @@
-package com.devsenior.cdiaz.c1m8_property_service;
+package com.devsenior.cdiaz.property.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class C1m8PropertyServiceApplication {
+public class PropertyServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(C1m8PropertyServiceApplication.class, args);
+		SpringApplication.run(PropertyServiceApplication.class, args);
 	}
 
 }
