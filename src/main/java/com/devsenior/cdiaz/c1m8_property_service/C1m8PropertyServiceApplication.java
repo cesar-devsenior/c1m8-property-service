b8fbddad8bf882872de78378b64c44c0faa53fab
Cesar Augusto Diaz Arriaga <cesardevsenior@gmail.com>
2025-08-06 20:58:27 -0500

    Initial Commit

---
+++ src/main/java/com/devsenior/cdiaz/c1m8_property_service/C1m8PropertyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.cdiaz.c1m8_property_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1m8PropertyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1m8PropertyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/cdiaz/c1m8_property_service/C1m8PropertyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.cdiaz.c1m8_property_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1m8PropertyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
